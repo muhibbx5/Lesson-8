1cade016d427d8cc11ae4a2d9dffc84cec745ca4
muhibbx5 <muhibbali56@gmail.com>
2024-10-27 13:48:07 +0500

    AND-OR-NOT-NOT EQUAL TO

---
+++ main.py
@@ -0,0 +1,21 @@
+d=21
+c=18
+e=19
+if c>d and e>d:
+ print("These numbers are greater than d")
+else:
+  print("These numbers are not greater than d")
+
+z=2
+x=3
+y=5
+if x>0 or y>0 or z>0:
+  print("These letters have a greater value than 0")
+else:
+  print("These letters DO NOT have a greater value than 0")
+
+a=31
+if not a:
+    print("true")
+else:
+    print("false")
